2804bc4a1451ffe3adc65a95623b46388ad2e523
Nathan Van Bignoot <contact@nathanvanbignoot.fr>
2025-04-11 23:15:24 +0200

    refactor: update astro + config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  output: "server",
+  output: "static",
   vite: {
     resolve: {
       alias: {
